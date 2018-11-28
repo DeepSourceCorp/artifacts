c04a09321d90f352634659a8600db4e60b9b59a1
Jai <jai@deepsource.io>
2018-11-28 18:03:12 +0530

    Removes secrets from marvin config. Picked from env
    Also adds processors to marvin config
    
    Signed-off-by: Jai <jai@deepsource.io>

--- types/marvin.go
+++ types/marvin.go
@@ -1,22 +1,19 @@
 package types
 
 type MarvinConfig struct {
-	MachineryTaskID string `toml:"taskID"`
-	RunID           string `toml:"runID"`
-	RunType         string `toml:"runType"`
-	RMQURL          string `toml:"rmqURL"`
-	RMQExchange     string `toml:"rmqExchange"`
-	RMQRoutingKey   string `toml:"rmqRoutingKey"`
-	CheckSeq        string `toml:"checkSeq"`
-	TaskType        string `toml:"taskType"`
-	TaskShortcode   string `toml:"taskShortcode"`
-	TaskCommand     string `toml:"taskCommand"`
-	DefaultHash     string `toml:"defaultHash"`
-	CheckoutHash    string `toml:"checkoutHash"`
+	MachineryTaskID   string   `toml:"taskID"`
+	RunID             string   `toml:"runID"`
+	RunType           string   `toml:"runType"`
+	RMQRoutingKey     string   `toml:"rmqRoutingKey"`
+	CheckSeq          string   `toml:"checkSeq"`
+	AnalyzerShortcode string   `toml:"analyzerShortcode"`
+	AnalyzerCommand   string   `toml:"analyzerCommand"`
+	DefaultHash       string   `toml:"defaultHash"`
+	CheckoutHash      string   `toml:"checkoutHash"`
+	Processors        []string `toml:"processors"`
 }
 
 type AnalysisConfig struct {
 	Files           []string `json:"files"`
 	ExcludePatterns []string `json:"exclude_patterns"`
-	Processors      []string `json:"processors"`
 }
