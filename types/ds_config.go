e805d390effb6c7df4b8dcc0e52f5eed1d9ce751
Jai <jai@deepsource.io>
2018-12-18 14:40:15 +0530

    [types] Adds runtime version to .deepsource.toml

--- types/ds_config.go
+++ types/ds_config.go
@@ -1,11 +1,13 @@
 package types
 
+// DSConfig is the struct for .deepsource.toml file
 type DSConfig struct {
 	Version         int      `toml:"version" json:"version"`
 	ExcludePatterns []string `toml:"exclude_patterns,omitempty" json:"exclude_patterns,omitempty"`
 	TestPatterns    []string `toml:"test_patterns,omitempty" json:"test_patterns,omitempty"`
 	Analyzers       []struct {
 		Name                string   `toml:"name" json:"name"`
+		RuntimeVersion      string   `toml:"runtime_version" json:"runtime_version"`
 		Enabled             bool     `toml:"enabled" json:"enabled"`
 		DependencyFilePaths []string `toml:"dependency_file_paths,omitempty" json:"dependency_file_paths,omitempty"`
 	} `toml:"analyzers,omitempty" json:"analyzers,omitempty"`
