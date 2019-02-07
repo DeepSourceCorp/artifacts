969ffa14f89b9fc49f4f05da0bab412a8dc16d08
Jaipradeesh <jai@deepsource.io>
2019-02-07 15:01:55 +0530

    [types] Adds meta to analyzers
    Signed-off-by: Jaipradeesh <jai@deepsource.io>

--- types/ds_config.go
+++ types/ds_config.go
@@ -6,9 +6,10 @@ type DSConfig struct {
 	ExcludePatterns []string `toml:"exclude_patterns,omitempty" json:"exclude_patterns,omitempty"`
 	TestPatterns    []string `toml:"test_patterns,omitempty" json:"test_patterns,omitempty"`
 	Analyzers       []struct {
-		Name                string   `toml:"name" json:"name"`
-		RuntimeVersion      string   `toml:"runtime_version,omitempty" json:"runtime_version,omitempty"`
-		Enabled             bool     `toml:"enabled" json:"enabled"`
-		DependencyFilePaths []string `toml:"dependency_file_paths,omitempty" json:"dependency_file_paths,omitempty"`
+		Name                string      `toml:"name" json:"name"`
+		RuntimeVersion      string      `toml:"runtime_version,omitempty" json:"runtime_version,omitempty"`
+		Enabled             bool        `toml:"enabled" json:"enabled"`
+		DependencyFilePaths []string    `toml:"dependency_file_paths,omitempty" json:"dependency_file_paths,omitempty"`
+		Meta                interface{} `toml:"meta,omitempty" json:"meta,omitempty"`
 	} `toml:"analyzers,omitempty" json:"analyzers,omitempty"`
 }
