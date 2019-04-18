bd001c50f76f66372ea14fb4a9effed8a0f3db06
Jai <jai@deepsource.io>
2019-04-17 19:01:32 -0700

    [types] Add exclude files to analysis config
    Signed-off-by: Jai <jai@deepsource.io>

--- types/marvin.go
+++ types/marvin.go
@@ -14,6 +14,7 @@ type MarvinConfig struct {
 type AnalysisConfig struct {
 	Files           []string    `json:"files"`
 	ExcludePatterns []string    `json:"exclude_patterns"`
+	ExcludeFiles    []string    `json:"exclude_files"`
 	TestFiles       []string    `json:"test_files"`
 	TestPatterns    []string    `json:"test_patterns"`
 	AnalyzerMeta    interface{} `json:"analyzer_meta"`
