17f6a25763161536a6852545d6a729883df0cc31
Sourya Vatsyayan <sourya@deepsource.io>
2020-03-02 17:36:26 +0530

    Separate out types, add types.proto file
    Signed-off-by: Sourya Vatsyayan <sourya@deepsource.io>

--- types/atlas_reception.go
+++ types/atlas_reception.go
@@ -2,98 +2,132 @@ package types
 
 // RepoRun type is the expected structure of a repo run task
 // to be received
+//proteus:generate
+type RepoRunVCSMeta struct {
+	RemoteURL   string `json:"remote_url"`
+	CheckoutOID string `json:"checkout_oid"`
+}
+
+//proteus:generate
 type RepoRun struct {
-	RunID     string `json:"run_id"`
-	RunSerial string `json:"run_serial"`
-	VCSMeta   struct {
-		RemoteURL   string `json:"remote_url"`
-		CheckoutOID string `json:"checkout_oid"`
-	} `json:"vcs_meta"`
+	RunID     string         `json:"run_id"`
+	RunSerial string         `json:"run_serial"`
+	VCSMeta   RepoRunVCSMeta `json:"vcs_meta"`
 }
 
 // Artifact is data sent via CLI and stored in s3
+//proteus:generate
+type Metadata struct {
+	WorkDir string `json:"workDir"`
+}
+
+//proteus:generate
 type Artifact struct {
-	Key      string `json:"key"`
-	URL      string `json:"url"`
-	Metadata struct {
-		WorkDir string `json:"workDir"`
-	} `json:"metadata"`
+	Key      string   `json:"key"`
+	URL      string   `json:"url"`
+	Metadata Metadata `json:"metadata"`
 }
 
 // AnalysisRun type is the expected structure of a analysis run task
 // to be received
-type AnalysisRun struct {
-	RunID           string `json:"run_id"`
-	RunSerial       string `json:"run_serial"`
-	DSConfigUpdated bool   `json:"ds_config_updated"`
-	VCSMeta         struct {
-		RemoteURL       string `json:"remote_url"`
-		BaseBranch      string `json:"base_branch"`
-		BaseOID         string `json:"base_oid"`
-		CheckoutOID     string `json:"checkout_oid"`
-		CloneSubmodules bool   `json:"clone_submodules"`
-	} `json:"vcs_meta"`
-	Keys struct {
-		SSH struct {
-			Public  string `json:"public"`
-			Private string `json:"private"`
-		} `json:"ssh,omitempty"`
-	} `json:"keys"`
-	Checks []struct {
-		CheckSeq     string     `json:"check_seq"`
-		Artifacts    []Artifact `json:"artifacts"`
-		AnalyzerMeta struct {
-			Shortcode   string `json:"name"`
-			Command     string `json:"command"`
-			Version     string `json:"version"`
-			CPULimit    string `json:"cpu_limit"`
-			MemoryLimit string `json:"memory_limit"`
-		} `json:"analyzer_meta"`
-		Processors []string `json:"processors"`
-	} `json:"checks"`
+//proteus:generate
+type AnalysisRunVCSMeta struct {
+	RemoteURL       string `json:"remote_url"`
+	BaseBranch      string `json:"base_branch"`
+	BaseOID         string `json:"base_oid"`
+	CheckoutOID     string `json:"checkout_oid"`
+	CloneSubmodules bool   `json:"clone_submodules"`
 }
 
+//proteus:generate
+type SSH struct {
+	Public  string `json:"public"`
+	Private string `json:"private"`
+}
+
+//proteus:generate
+type Keys struct {
+	SSH SSH `json:"ssh,omitempty"`
+}
+
+//proteus:generate
+type AnalyzerMeta struct {
+	Shortcode   string `json:"name"`
+	Command     string `json:"command"`
+	Version     string `json:"version"`
+	CPULimit    string `json:"cpu_limit"`
+	MemoryLimit string `json:"memory_limit"`
+}
+
+//proteus:generate
+type Check struct {
+	CheckSeq     string       `json:"check_seq"`
+	Artifacts    []Artifact   `json:"artifacts"`
+	AnalyzerMeta AnalyzerMeta `json:"analyzer_meta"`
+	Processors   []string     `json:"processors"`
+}
+
+//proteus:generate
+type AnalysisRun struct {
+	RunID           string             `json:"run_id"`
+	RunSerial       string             `json:"run_serial"`
+	DSConfigUpdated bool               `json:"ds_config_updated"`
+	VCSMeta         AnalysisRunVCSMeta `json:"vcs_meta"`
+	Keys            Keys               `json:"keys"`
+	Checks          []Check            `json:"checks"`
+}
+
+//proteus:generate
+type AutofixVCSMeta struct {
+	RemoteURL       string `json:"remote_url"`
+	BaseBranch      string `json:"base_branch"`
+	CheckoutOID     string `json:"checkout_oid"`
+	CloneSubmodules bool   `json:"clone_submodules"`
+}
+
+//proteus:generate
+type AutofixMeta struct {
+	Shortcode   string `json:"name"`
+	Command     string `json:"command"`
+	Version     string `json:"version"`
+	CPULimit    string `json:"cpu_limit"`
+	MemoryLimit string `json:"memory_limit"`
+}
+
+//proteus:generate
+type Autofixer struct {
+	AutofixMeta AutofixMeta    `json:"autofix_meta"`
+	Autofixes   []AutofixIssue `json:"autofixes"`
+}
+
+//proteus:generate
 type AutofixRun struct {
-	RunID     string `json:"run_id"`
-	RunSerial string `json:"run_serial"`
-	VCSMeta   struct {
-		RemoteURL       string `json:"remote_url"`
-		BaseBranch      string `json:"base_branch"`
-		CheckoutOID     string `json:"checkout_oid"`
-		CloneSubmodules bool   `json:"clone_submodules"`
-	} `json:"vcs_meta"`
-	Keys struct {
-		SSH struct {
-			Public  string `json:"public"`
-			Private string `json:"private"`
-		} `json:"ssh,omitempty"`
-	} `json:"keys"`
-	Autofixer struct {
-		AutofixMeta struct {
-			Shortcode   string `json:"name"`
-			Command     string `json:"command"`
-			Version     string `json:"version"`
-			CPULimit    string `json:"cpu_limit"`
-			MemoryLimit string `json:"memory_limit"`
-		} `json:"autofix_meta"`
-		Autofixes []AutofixIssue `json:"autofixes"`
-	} `json:"autofixer"`
+	RunID     string         `json:"run_id"`
+	RunSerial string         `json:"run_serial"`
+	VCSMeta   AutofixVCSMeta `json:"vcs_meta"`
+	Keys      Keys           `json:"keys"`
+	Autofixer Autofixer      `json:"autofixer"`
 }
 
 // CancelCheckRun type is the expected structure of a check cancellation
 // task to be recieved
-type CancelCheckRun struct {
-	AnalysisMeta struct {
-		RunID     string `json:"run_id"`
-		RunSerial string `json:"run_serial"`
-		CheckSeq  string `json:"check_seq"`
-	} `json:"analysis_meta"`
+//proteus:generate
+type CancelCheckAnalysisMeta struct {
 	RunID     string `json:"run_id"`
 	RunSerial string `json:"run_serial"`
+	CheckSeq  string `json:"check_seq"`
+}
+
+//proteus:generate
+type CancelCheckRun struct {
+	AnalysisMeta CancelCheckAnalysisMeta `json:"analysis_meta"`
+	RunID        string                  `json:"run_id"`
+	RunSerial    string                  `json:"run_serial"`
 }
 
 // Beacon type is the expected structure of a beacon task
 // to be received
+//proteus:generate
 type BeaconRun struct {
 	RunID        string `json:"run_id"`
 	RepositoryID int64  `json:"repository_id"`
