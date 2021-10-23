a0e7c6ebdc65f6e8b9df708c5f928237d8b36208
Siddhant N Trivedi <siddhant@deepsource.io>
2021-10-23 22:40:45 +0530

    add fibonacci util funcs
    Signed-off-by: Siddhant N Trivedi <siddhant@deepsource.io>

--- utils.go
+++ utils.go
@@ -67,3 +67,22 @@ func GetNewHTTPClient(certFilePath string) (*http.Client, error) {
 
 	return httpClient, nil
 }
+
+// Fibonacci returns successive Fibonacci numbers starting from 1
+func fibonacci() func() int {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+}
+
+// FibonacciNext returns next number in Fibonacci sequence greater than start
+func FibonacciNextNum(start int) int {
+	fib := fibonacci()
+	num := fib()
+	for num <= start {
+		num = fib()
+	}
+	return num
+}
