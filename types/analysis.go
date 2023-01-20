3fcb1ffe4a7c69570dd115de2d88e7fd1a99737c
Akshit Garg <akshit@deepsource.io>
2023-01-20 16:25:05 +0530

    Add base branch to marvin config
    Signed-off-by: Akshit Garg <akshit@deepsource.io>

--- types/analysis.go
+++ types/analysis.go
@@ -117,6 +117,7 @@ type MarvinAnalysisConfig struct {
 	DSConfigUpdated            bool             `toml:"dsConfigUpdated"`
 	Processors                 []string         `toml:"processors"`
 	DiffMetaCommits            []DiffMetaCommit `toml:"diffMetaCommits"`
+	BaseBranch                 string           `toml:"baseBranch"`
 }
 
 //proteus:generate
