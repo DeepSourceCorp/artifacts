7d67e0544805dfb373bc61adb6f2e0edc48c9a86
Akshit Garg <akshit@deepsource.io>
2023-04-10 17:36:20 +0530

    Add Git patch
    Signed-off-by: Akshit Garg <akshit@deepsource.io>

--- types/atlas_reception.go
+++ types/atlas_reception.go
@@ -34,6 +34,10 @@ type AnalysisRunVCSMeta struct {
 	CheckoutOID                string `json:"checkout_oid"`
 	IsForDefaultAnalysisBranch bool   `json:"is_for_default_analysis_branch"`
 	CloneSubmodules            bool   `json:"clone_submodules"`
+
+	// IDE
+	GitPatch string `json:"git_patch"`
+	IsIDE    bool   `json:"is_ide"`
 }
 
 //proteus:generate
