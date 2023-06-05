f153b49240b5b49827f8cdbded37d6cccdaf0028
Akshit Garg <akshit@deepsource.io>
2023-06-05 12:40:13 +0530

    Move is_ide to analysis_config
    Signed-off-by: Akshit Garg <akshit@deepsource.io>

--- types/analysis.go
+++ types/analysis.go
@@ -117,7 +117,6 @@ type MarvinAnalysisConfig struct {
 	DSConfigUpdated            bool             `toml:"dsConfigUpdated"`
 	Processors                 []string         `toml:"processors"`
 	DiffMetaCommits            []DiffMetaCommit `toml:"diffMetaCommits"`
-	IsIDE                      string           `toml:"is_ide"`
 }
 
 //proteus:generate
