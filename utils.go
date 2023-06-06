13925a18cd4dbed320f6818d9c2c4822b87bb474
Siddhant N Trivedi <siddhant@deepsource.io>
2023-06-06 11:34:17 +0530

    chore: print bool as string
    Signed-off-by: Siddhant N Trivedi <siddhant@deepsource.io>

--- utils.go
+++ utils.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -195,7 +196,7 @@ type AnalysisEventsLog struct {
 	Repository    string
 	Shortcode     string
 	CommitSHA     string
-	IsFullRun     string
+	IsFullRun     bool
 }
 
 // logAnalysisEventTimestamp logs the timestamp at various analysis stages.
@@ -208,7 +209,7 @@ func (a *AnalysisEventsLog) LogAnalysisEventTimestamp(runType, stage string) {
 		a.Shortcode,
 		a.Repository,
 		a.CommitSHA,
-		a.IsFullRun,
+		strconv.FormatBool(a.IsFullRun),
 		stage,
 		time.Now().Unix(),
 	)
