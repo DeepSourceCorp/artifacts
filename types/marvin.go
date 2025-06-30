093ffee79feabac93d45d0a9070fa5ead5d7bc27
Anto Christopher <93177734+anto-deepsource@users.noreply.github.com>
2025-06-30 12:55:38 +0530

    chore: add plaintext object URLs (#217)
    * chore: add plaintext object URLs
    
    Signed-off-by: Anto Christopher <93177734+anto-deepsource@users.noreply.github.com>
    
    * style: format code with Gofumpt
    
    This commit fixes the style issues introduced in 0e2c4e5 according to the output
    from Gofumpt.
    
    Details: https://github.com/DeepSourceCorp/artifacts/pull/217
    
    ---------
    
    Signed-off-by: Anto Christopher <93177734+anto-deepsource@users.noreply.github.com>
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- types/marvin.go
+++ types/marvin.go
@@ -4,8 +4,10 @@ package types
 type MarvinCacheURLs struct {
 	Enabled bool `toml:"enabled"`
 
-	MetadataDownload string `toml:"metadataDL"`
-	MetadataUpload   string `toml:"metadataUL"`
-	CacheDownload    string `toml:"cacheDL"`
-	CacheUpload      string `toml:"cacheUL"`
+	MetadataDownload       string `toml:"metadataDL"`
+	MetadataUpload         string `toml:"metadataUL"`
+	CacheDownload          string `toml:"cacheDL"`
+	CacheUpload            string `toml:"cacheUL"`
+	PlaintextCacheDownload string `toml:"plaintextCacheDL"`
+	PlaintextCacheUpload   string `toml:"plaintextCacheUL"`
 }
