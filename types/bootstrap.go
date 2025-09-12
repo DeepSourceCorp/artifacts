f95bee3e0d59ed83f929290227d7dab19da7d289
Vishnu Jayadevan <vishnu@deepsource.io>
2025-09-12 14:24:10 +0530

    feat: add bootstrap configuration

---
+++ types/bootstrap.go
@@ -0,0 +1,18 @@
+package types
+
+import "encoding/json"
+
+type SourceMetadataGCS struct {
+	ObjectName          string `json:"object_name,omitempty"`
+	BucketName          string `json:"bucket_name,omitempty"`
+	UseWorkloadIdentity bool   `json:"use_workload_identity,omitempty"`
+	CredentialsJSON     string `json:"credentials_json,omitempty"`
+}
+
+type BootstrapConfig struct {
+	RepositoryID   string          `json:"repository_id"`
+	SourceType     string          `json:"source_type"`
+	SourceMetadata json.RawMessage `json:"source_metadata"`
+	CodePath       string          `json:"code_path"`
+	PVCName        string          `json:"pvc_name"`
+}
