282b66e0db74a7a5fc6b919a05d8e14b1eb2d785
Vishnu Jayadevan <vishnu@deepsource.io>
2026-01-12 16:28:25 +0530

    fix: update marvin analysis config

--- types/analysis.go
+++ types/analysis.go
@@ -116,20 +116,21 @@ type CancelCheckResultCeleryTask struct {
 
 //proteus:generate
 type MarvinAnalysisConfig struct {
-	RunID                      string           `toml:"runID"`
-	RunSerial                  string           `toml:"runSerial"`
-	CheckSeq                   string           `toml:"checkSeq"`
-	AnalyzerShortcode          string           `toml:"analyzerShortcode"`
-	AnalyzerCommand            string           `toml:"analyzerCommand"`
-	AnalyzerType               string           `toml:"analyzerType"`
-	BaseOID                    string           `toml:"baseOID"`
-	CheckoutOID                string           `toml:"checkoutOID"`
-	Repository                 string           `toml:"repository"`
-	IsFullRun                  bool             `toml:"is_full_run"`
-	IsForDefaultAnalysisBranch bool             `toml:"isForDefaultAnalysisBranch"`
-	DSConfigUpdated            bool             `toml:"dsConfigUpdated"`
-	Processors                 []string         `toml:"processors"`
-	DiffMetaCommits            []DiffMetaCommit `toml:"diffMetaCommits"`
+	RunID                         string           `toml:"runID"`
+	RunSerial                     string           `toml:"runSerial"`
+	CheckSeq                      string           `toml:"checkSeq"`
+	AnalyzerShortcode             string           `toml:"analyzerShortcode"`
+	AnalyzerCommand               string           `toml:"analyzerCommand"`
+	AnalyzerType                  string           `toml:"analyzerType"`
+	BaseOID                       string           `toml:"baseOID"`
+	CheckoutOID                   string           `toml:"checkoutOID"`
+	Repository                    string           `toml:"repository"`
+	IsFullRun                     bool             `toml:"is_full_run"`
+	IsForDefaultAnalysisBranch    bool             `toml:"isForDefaultAnalysisBranch"`
+	DSConfigUpdated               bool             `toml:"dsConfigUpdated"`
+	Processors                    []string         `toml:"processors"`
+	DiffMetaCommits               []DiffMetaCommit `toml:"diffMetaCommits"`
+	PreviousCompletedRunCommitOID string           `toml:"previousCompletedRunCommitOID"`
 }
 
 //proteus:generate
